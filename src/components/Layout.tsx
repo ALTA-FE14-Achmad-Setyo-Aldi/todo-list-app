1df3bf1ee8ae33e110a517557246e5f3691e2142
setyoaldi <achmadsetyoaldi@gmail.com>
2023-05-31 15:56:42 +0700

    feat: add footer

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,7 @@
 import { FC, ReactNode } from "react";
 
 import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 interface Props {
   children: ReactNode;
@@ -8,11 +9,12 @@ interface Props {
 
 const Layout: FC<Props> = (props) => {
   return (
-    <div className="w-full h-screen overflow-auto flex flex-col bg-gradient-to-r from-[#83a4d4] to-[#b6fbff] dark:from-[#141E30] dark:to-[#243B55]">
+    <div className="w-full h-screen flex flex-col overflow-auto bg-gradient-to-r from-[#83a4d4] to-[#b6fbff] dark:from-[#141E30] dark:to-[#465669]">
       <Navbar />
-      <div className="h-full w-full py-3 px-3 flex flex-col items-center justify-center ">
+      <div className="h-full w-full py-3 px-3 mb-10 flex flex-col items-center justify-center ">
         {props.children}
       </div>
+      <Footer />
     </div>
   );
 };
